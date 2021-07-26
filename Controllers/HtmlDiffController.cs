e4d218b75f9bc3d5582e801bd003834df67dfdce
spq3658 <spq3658@PC0GL1N1>
2021-07-26 12:26:38 +0530

    Add project files.

---
+++ Controllers/HtmlDiffController.cs
@@ -0,0 +1,82 @@
+﻿using Aspose.Html;
+using Aspose.Words;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HtmlTableDiffController : ControllerBase
+    {
+        [HttpPost]
+        public string Post([FromBody] JsonData content)
+        {
+            Aspose.Words.License license = new Aspose.Words.License();
+            string nameFormat = DateTime.UtcNow.ToFileTimeUtc().ToString();
+            Console.WriteLine("Date time = " + DateTime.UtcNow.ToFileTimeUtc().ToString());
+            //JsonData.SetLicense(license);
+            license.SetLicense(Directory.GetCurrentDirectory() + "\\Aspose.Total.lic");
+            //license.SetLicense("/Aspose.Total.lic");
+
+
+
+            string documentPath1 = Directory.GetCurrentDirectory() + $"\\1_{nameFormat}.html";
+            string documentPath2 = Directory.GetCurrentDirectory() + $"\\2_{nameFormat}.html";
+            string comparisonDocument = Directory.GetCurrentDirectory() + $"\\Output_{nameFormat}.html";
+      
+            JsonData.CreateFile(documentPath1, content.content1);
+            JsonData.CreateFile(documentPath2, content.content2);
+
+            int added = 0;
+            int deleted = 0;
+
+            // Load both documents in Aspose.Words
+            Document doc1 = new Document(documentPath1);
+            Document doc2 = new Document(documentPath2);
+            Document docComp = new Document(documentPath1);
+
+            DocumentBuilder builder = new DocumentBuilder(docComp);
+            doc1.Compare(doc2, "a", DateTime.Now);
+
+            foreach (Revision revision in doc1.Revisions)
+            {
+
+                switch (revision.RevisionType)
+                {
+
+                    case RevisionType.Insertion:
+                        added++;
+                        break;
+
+                    case RevisionType.Deletion:
+                        deleted++;
+                        break;
+
+                }
+
+                Console.WriteLine(revision.RevisionType + ": " + revision.ParentNode);
+            }
+
+         
+            doc1.Save(comparisonDocument, SaveFormat.Html);
+            var text = "";
+            using (var document = new HTMLDocument(comparisonDocument))
+            {
+                text = document.DocumentElement.InnerHTML;
+            }
+            Console.WriteLine("\n the difference =  " + text);
+
+            //Console.WriteLine("The content = " + content);
+            JsonData.DeleteFiles(documentPath1);
+            JsonData.DeleteFiles(documentPath2);
+            JsonData.DeleteFiles(comparisonDocument);
+            return text;
+        }
+    }
+}
---
+++ JsonData.cs
@@ -0,0 +1,34 @@
+﻿using Aspose.Words;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class JsonData
+    {
+        public string content1 { get; set; }
+        public string content2 { get; set; }
+        public static void CreateFile(string documentPath, string dataInFile)
+        {
+            File.WriteAllText(documentPath, dataInFile);
+
+        }
+
+        internal static void SetLicense(License license)
+        {
+            license.SetLicense("C:/Users/spq3658/source/repos/WebApplication1/Aspose.Total.lic");
+        }
+
+        internal static void DeleteFiles(string documentPath)
+        {
+            if (File.Exists(documentPath))
+            {
+                File.Delete(documentPath);
+            }
+
+        }
+    }  
+}
